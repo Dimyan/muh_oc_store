fd2d5df6ae03fe259da24dd237dae934ea8a55f1
Muhovyaz <muhovyaz@gmail.com>
2012-11-15 12:04:17 +0700

    delete cache files and remove db_prefix

--- src/admin/config.php
+++ src/admin/config.php
@@ -1,32 +1,32 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://flyfishingstore.ru/admin/');
-define('HTTP_CATALOG', 'http://flyfishingstore.ru/');
-define('HTTP_IMAGE', 'http://flyfishingstore.ru/image/');
+define('HTTP_SERVER', 'http://muh-store/admin/');
+define('HTTP_CATALOG', 'http://muh-store/');
+define('HTTP_IMAGE', 'http://muh-store/image/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://flyfishingstore.ru/admin/');
-define('HTTPS_CATALOG', 'http://flyfishingstore.ru/');
-define('HTTPS_IMAGE', 'http://flyfishingstore.ru/image/');
+define('HTTPS_SERVER', 'http://muh-store/admin/');
+define('HTTPS_CATALOG', 'http://muh-store/');
+define('HTTPS_IMAGE', 'http://muh-store/image/');
 
 // DIR
-define('DIR_APPLICATION', '/home6/flyfisi3/public_html/oc_store/admin/');
-define('DIR_SYSTEM', '/home6/flyfisi3/public_html/oc_store/system/');
-define('DIR_DATABASE', '/home6/flyfisi3/public_html/oc_store/system/database/');
-define('DIR_LANGUAGE', '/home6/flyfisi3/public_html/oc_store/admin/language/');
-define('DIR_TEMPLATE', '/home6/flyfisi3/public_html/oc_store/admin/view/template/');
-define('DIR_CONFIG', '/home6/flyfisi3/public_html/oc_store/system/config/');
-define('DIR_IMAGE', '/home6/flyfisi3/public_html/oc_store/image/');
-define('DIR_CACHE', '/home6/flyfisi3/public_html/oc_store/system/cache/');
-define('DIR_DOWNLOAD', '/home6/flyfisi3/public_html/oc_store/download/');
-define('DIR_LOGS', '/home6/flyfisi3/public_html/oc_store/system/logs/');
-define('DIR_CATALOG', '/home6/flyfisi3/public_html/oc_store/catalog/');
+define('DIR_APPLICATION', '/var/www/muh_oc_store/src/admin/');
+define('DIR_SYSTEM', '/var/www/muh_oc_store/src/system/');
+define('DIR_DATABASE', '/var/www/muh_oc_store/src/system/database/');
+define('DIR_LANGUAGE', '/var/www/muh_oc_store/src/admin/language/');
+define('DIR_TEMPLATE', '/var/www/muh_oc_store/src/admin/view/template/');
+define('DIR_CONFIG', '/var/www/muh_oc_store/src/system/config/');
+define('DIR_IMAGE', '/var/www/muh_oc_store/src/image/');
+define('DIR_CACHE', '/var/www/muh_oc_store/src/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/muh_oc_store/src/download/');
+define('DIR_LOGS', '/var/www/muh_oc_store/src/system/logs/');
+define('DIR_CATALOG', '/var/www/muh_oc_store/src/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'flyfisi3_muh');
-define('DB_PASSWORD', 'mkG;wOR?cB');
-define('DB_DATABASE', 'flyfisi3_muh_oc');
-define('DB_PREFIX', 'oc');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'security');
+define('DB_DATABASE', 'muh_oc_store');
+define('DB_PREFIX', '');
 ?>
--- src/config.php
+++ src/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://flyfishingstore.ru/');
-define('HTTP_IMAGE', 'http://flyfishingstore.ru/image/');
-define('HTTP_ADMIN', 'http://flyfishingstore.ru/admin/');
+define('HTTP_SERVER', 'http://muh-store/');
+define('HTTP_IMAGE', 'http://muh-store/image/');
+define('HTTP_ADMIN', 'http://muh-store/admin/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://flyfishingstore.ru/');
-define('HTTPS_IMAGE', 'http://flyfishingstore.ru/image/');
+define('HTTPS_SERVER', 'http://muh-store/');
+define('HTTPS_IMAGE', 'http://muh-store/image/');
 
 // DIR
-define('DIR_APPLICATION', '/home6/flyfisi3/public_html/oc_store/catalog/');
-define('DIR_SYSTEM', '/home6/flyfisi3/public_html/oc_store/system/');
-define('DIR_DATABASE', '/home6/flyfisi3/public_html/oc_store/system/database/');
-define('DIR_LANGUAGE', '/home6/flyfisi3/public_html/oc_store/catalog/language/');
-define('DIR_TEMPLATE', '/home6/flyfisi3/public_html/oc_store/catalog/view/theme/');
-define('DIR_CONFIG', '/home6/flyfisi3/public_html/oc_store/system/config/');
-define('DIR_IMAGE', '/home6/flyfisi3/public_html/oc_store/image/');
-define('DIR_CACHE', '/home6/flyfisi3/public_html/oc_store/system/cache/');
-define('DIR_DOWNLOAD', '/home6/flyfisi3/public_html/oc_store/download/');
-define('DIR_LOGS', '/home6/flyfisi3/public_html/oc_store/system/logs/');
+define('DIR_APPLICATION', '/var/www/muh_oc_store/src/catalog/');
+define('DIR_SYSTEM', '/var/www/muh_oc_store/src/system/');
+define('DIR_DATABASE', '/var/www/muh_oc_store/src/system/database/');
+define('DIR_LANGUAGE', '/var/www/muh_oc_store/src/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/muh_oc_store/src/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/muh_oc_store/src/system/config/');
+define('DIR_IMAGE', '/var/www/muh_oc_store/src/image/');
+define('DIR_CACHE', '/var/www/muh_oc_store/src/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/muh_oc_store/src/download/');
+define('DIR_LOGS', '/var/www/muh_oc_store/src/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'flyfisi3_muh');
-define('DB_PASSWORD', 'mkG;wOR?cB');
-define('DB_DATABASE', 'flyfisi3_muh_oc');
-define('DB_PREFIX', 'oc');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'security');
+define('DB_DATABASE', 'muh_oc_store');
+define('DB_PREFIX', '');
 ?>
